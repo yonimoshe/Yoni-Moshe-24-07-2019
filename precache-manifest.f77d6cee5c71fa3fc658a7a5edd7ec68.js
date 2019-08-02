b2f8d418f556df310786812d60acba0c5f66cdb4
Yoni Moshe <42314457+yonimoshe@users.noreply.github.com>
2019-08-02 12:51:23 +0300

    static build upload

---
+++ precache-manifest.f77d6cee5c71fa3fc658a7a5edd7ec68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f19e412919de991b4b60a18b3c3983",
+    "url": "/Yoni-Moshe-24-07-2019/index.html"
+  },
+  {
+    "revision": "5a5274406a6abc00e8ac",
+    "url": "/Yoni-Moshe-24-07-2019/static/css/main.ff919f0b.chunk.css"
+  },
+  {
+    "revision": "f18e66667eb82202ff3f",
+    "url": "/Yoni-Moshe-24-07-2019/static/js/2.081fbd73.chunk.js"
+  },
+  {
+    "revision": "5a5274406a6abc00e8ac",
+    "url": "/Yoni-Moshe-24-07-2019/static/js/main.536e78d6.chunk.js"
+  },
+  {
+    "revision": "360c1d0fdfedfd9cda5a",
+    "url": "/Yoni-Moshe-24-07-2019/static/js/runtime~main.1c5b6d95.js"
+  }
+]);
