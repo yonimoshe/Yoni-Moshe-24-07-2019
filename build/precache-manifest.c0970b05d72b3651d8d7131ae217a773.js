eb83f56020672f8c0b927a2d3736ae348a9c4601
Yoni Moshe <42314457+yonimoshe@users.noreply.github.com>
2019-08-21 20:46:38 +0300

    responsive fix

---
+++ build/precache-manifest.c0970b05d72b3651d8d7131ae217a773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247adeaadc231e47272d159754f2eb5b",
+    "url": "/Yoni-Moshe-24-07-2019/index.html"
+  },
+  {
+    "revision": "2de982a3c4d34652b05d",
+    "url": "/Yoni-Moshe-24-07-2019/static/css/main.7afc8ca6.chunk.css"
+  },
+  {
+    "revision": "f18e66667eb82202ff3f",
+    "url": "/Yoni-Moshe-24-07-2019/static/js/2.081fbd73.chunk.js"
+  },
+  {
+    "revision": "2de982a3c4d34652b05d",
+    "url": "/Yoni-Moshe-24-07-2019/static/js/main.7d718ab9.chunk.js"
+  },
+  {
+    "revision": "360c1d0fdfedfd9cda5a",
+    "url": "/Yoni-Moshe-24-07-2019/static/js/runtime~main.1c5b6d95.js"
+  }
+]);
